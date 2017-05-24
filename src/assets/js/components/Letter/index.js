1b7d582ebf71bad10aabde026f19f6dc31432a27
giulio collesei <giulio.collesei+gitlab@aquest.it>
2017-05-24 18:32:04 +0200

    draw line

--- src/assets/js/components/Letter/index.js
+++ src/assets/js/components/Letter/index.js
@@ -9,12 +9,6 @@ class Letter extends Component {
 
     B = 'M65.6,30.8c-1,2.8-2.4,5.4-4.2,7.6c-1.8,2.2-3.8,4.2-6.2,5.9c-2.3,1.7-4.8,3-7.5,3.9c2.5,0.5,4.8,1.4,7,2.5c2.2,1.1,4.1,2.6,5.8,4.4c1.6,1.8,2.9,3.9,3.9,6.2c1,2.4,1.4,5.1,1.4,8.1c0,4.6-1,8.7-2.9,12.5c-1.9,3.8-4.5,7-7.9,9.7s-7.3,4.7-11.8,6.2c-4.5,1.5-9.4,2.2-14.6,2.2H0L17.7,0H41c4.5,0,8.4,0.5,11.6,1.6c3.3,1.1,6,2.6,8.2,4.5c2.1,1.9,3.7,4.2,4.8,6.8c1,2.7,1.6,5.5,1.6,8.6C67.1,24.9,66.6,28,65.6,30.8z M50.3,57.7c-4-2.9-9.3-4.4-16.1-4.4H17.7l-6.6,38.2h18.8c3.8,0,7.4-0.5,10.6-1.4c3.2-1,6-2.4,8.4-4.2c2.3-1.8,4.2-4.1,5.5-6.7c1.3-2.6,2-5.7,2-9.2C56.3,64.7,54.3,60.6,50.3,57.7zM52.9,12.3C49.8,9.8,45,8.5,38.6,8.5h-13l-6.4,36.3h13.2c3.9,0,7.5-0.6,10.6-1.7c3.1-1.1,5.8-2.7,7.9-4.7c2.1-2,3.8-4.3,4.9-6.8c1.1-2.6,1.7-5.3,1.7-8.2C57.5,18.5,56,14.9,52.9,12.3z'
 
-    operation = {
-        m: 'moveTo',
-        l: 'liteTo',
-        c: 'bezierCurveTo'
-    }
-
     componentDidMount() {
         // Setup things
         this.app = new PIXI.Application(window.innerWidth, window.innerHeight, { // eslint-disable-line no-undef
@@ -25,16 +19,10 @@ class Letter extends Component {
         this.canvas = this.app.view;
         this.canvasContainer.appendChild(this.canvas);
 
-        const graphicsFromPath = new PathToGraphics(this.B);
-        console.log(graphicsFromPath.getGraphics());
-
-        const graphics = new PIXI.Graphics();
-        graphics.beginFill(0xFFFFFF);
-        // parsedPath.forEach(p => {
-        //     const [name, ...args] = p;
-        //     graphics[this.operation[name.toLowerCase()]](...args);
-        // });
-        graphics.endFill();
+        const graphicsFromPath = new PathToGraphics(this.B, {
+            fill: 0xFFFFFF
+        });
+        const graphics = graphicsFromPath.getGraphics();
 
         this.app.stage.addChild(graphics);
     }
--- src/assets/js/shared/PathToGraphics.js
+++ src/assets/js/shared/PathToGraphics.js
@@ -1,23 +1,237 @@
 import 'pixi.js';
-import parse from 'parse-svg-path';
-import normalize from 'normalize-svg-path';
+import parse from 'svg-path-parser';
+// import normalize from 'normalize-svg-path';
+
+type Settings = {
+    fill: number
+};
+
+type Coords = {
+    x?: number,
+    y?: number,
+    x1?: number,
+    x2?: number,
+    y1?: number,
+    y2?: number,
+};
 
 class PathToGraphics {
 
-    constructor(path) {
-        this.path = normalize(parse(path));
-        this.lastCoord = {
+    path: Array<Object>;
+    settings: Settings;
+    lastCoords: Coords;
+    firstCoords: Coords;
+    firstClose: boolean;
+    graphics: Object;
+
+    defaultSettings = {
+        fill: 0x000000
+    }
+
+    static getCommandAPI(commandType: string): string {
+
+        const commandAPI = {
+            m: 'moveTo',
+            l: 'lineTo',
+            v: 'lineTo',
+            h: 'lineTo',
+            c: 'bezierCurveTo',
+            s: 'bezierCurveTo',
+            z: 'moveTo'
+        };
+
+        const lowerCommandType = commandType.toLowerCase();
+        return commandAPI[lowerCommandType] ? commandAPI[lowerCommandType] : '';
+
+    }
+
+    constructor(path: string, settings: Object) {
+        this.path = parse(path);
+        this.settings = Object.assign(this.defaultSettings, settings);
+        this.lastCoords = {
             x: 0,
             y: 0
         };
+        this.lastControl = {
+            x: 0,
+            y: 0
+        };
+        this.firstClose = true;
+        this.firstCoords = {};
+        this.graphics = new PIXI.Graphics(); // eslint-disable-line no-undef
     }
 
     getGraphics() {
-        console.log(this.path);
-        this.path.forEach((path, pIndex) => {
-            const [command, ...args] = path;
 
+        this.graphics.beginFill(this.settings.fill);
+
+        this.path.forEach(path => {
+            const {code, command, relative, ...args} = path;
+            const commandAPI = PathToGraphics.getCommandAPI(code);
+
+            this.drawPath(code, relative, args);
+
+            if (commandAPI.length === 0) {
+                throw new Error('commandAPI is empty', command);
+            }
         });
+
+        this.graphics.endFill();
+
+        return this.graphics;
+    }
+
+    drawPath(commandType: string, relative: boolean, args: Coords) {
+
+        const params = {
+
+            m: () => {
+                const coords = {
+                    x: relative ? args.x + this.lastCoords.x : args.x,
+                    y: relative ? args.y + this.lastCoords.y : args.y
+                };
+
+                this.graphics.moveTo(
+                    coords.x,
+                    coords.y
+                );
+
+                Object.assign(this.lastCoords, coords);
+
+                // first coords
+                if (typeof this.firstCoords.x === 'undefined') {
+                    Object.assign(this.firstCoords, coords);
+                }
+            },
+
+            l: () => {
+                const coords = {
+                    x: relative ? args.x + this.lastCoords.x : args.x,
+                    y: relative ? args.y + this.lastCoords.y : args.y
+                };
+
+                this.graphics.lineTo(
+                    coords.x,
+                    coords.y
+                );
+
+                Object.assign(this.lastCoords, coords);
+            },
+
+            v: () => {
+                const coords = {
+                    x: this.lastCoords.x,
+                    y: args.y
+                };
+
+                this.graphics.lineTo(
+                    coords.x,
+                    coords.y
+                );
+
+                Object.assign(this.lastCoords, coords);
+            },
+
+            h: () => {
+                const coords = {
+                    x: args.x,
+                    y: this.lastCoords.y
+                };
+
+                this.graphics.lineTo(
+                    coords.x,
+                    coords.y
+                );
+
+                Object.assign(this.lastCoords, coords);
+            },
+
+            c: () => {
+                const coords = {
+                    x1: relative ? args.x1 + this.lastCoords.x : args.x1,
+                    y1: relative ? args.y1 + this.lastCoords.y : args.y1,
+                    x2: relative ? args.x2 + this.lastCoords.x : args.x2,
+                    y2: relative ? args.y2 + this.lastCoords.y : args.y2,
+                    x: relative ? args.x + this.lastCoords.x : args.x, // destination point
+                    y: relative ? args.y + this.lastCoords.y : args.y // destination point
+                };
+
+                this.graphics.bezierCurveTo(
+                    coords.x1,
+                    coords.y1,
+                    coords.x2,
+                    coords.y2,
+                    coords.x,
+                    coords.y
+                );
+
+                Object.assign(this.lastCoords, {
+                    x: coords.x,
+                    y: coords.y
+                });
+
+                Object.assign(this.lastControl, {
+                    x: coords.x2,
+                    y: coords.y2
+                });
+            },
+
+            s: () => {
+                const coords = {
+                    x1: (this.lastCoords.x * 2) - this.lastControl.x,
+                    y1: (this.lastCoords.y * 2) - this.lastControl.y,
+                    x2: relative ? args.x2 + this.lastCoords.x : args.x2,
+                    y2: relative ? args.y2 + this.lastCoords.y : args.y2,
+                    x: relative ? args.x + this.lastCoords.x : args.x, // destination point
+                    y: relative ? args.y + this.lastCoords.y : args.y // destination point
+                };
+
+                this.graphics.bezierCurveTo(
+                    coords.x1,
+                    coords.y1,
+                    coords.x2,
+                    coords.y2,
+                    coords.x,
+                    coords.y
+                );
+
+                Object.assign(this.lastCoords, {
+                    x: coords.x,
+                    y: coords.y
+                });
+
+                Object.assign(this.lastControl, {
+                    x: coords.x2,
+                    y: coords.y2
+                });
+
+            },
+
+            z: () => {
+                const coords = Object.assign({}, this.firstCoords);
+
+                this.graphics
+                    .closePath();
+
+                if (!this.firstClose) {
+                    console.log('its not the first close');
+                    this.graphics.addHole();
+                }
+
+                Object.assign(this.lastCoords, coords);
+                this.firstClose = false;
+            }
+
+        };
+
+        const commandTypeLower = commandType.toLowerCase();
+
+        if (params[commandTypeLower]) {
+            params[commandTypeLower]();
+        } else {
+            console.log('Parameter not found!');
+        }
+
     }
 
 }
